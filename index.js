f90f3db7bd2cd78a5630e1915e392b4aa30d04ff
Jérémy Lal <kapouer@melix.org>
2017-04-15 17:39:18 +0200

    Disable if bps is <= 0, restore resume function

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 module.exports = function(bps) {
 	return function(req, res, next) {
-		var total = 0;
-		var resume = req.socket.resume;
-		req.socket.resume = function() {};
+		if (bps > 0) {
+			var total = 0;
+			var resume = req.socket.resume;
+			req.socket.resume = function() {};
 
-		var pulse = setInterval(function() {
-			total = total - bps / 100;
-			if (total < bps) {
-				resume.call(req.socket);
-			}
-		}, 10);
+			var pulse = setInterval(function() {
+				total = total - bps / 100;
+				if (total < bps) {
+					resume.call(req.socket);
+				}
+			}, 10);
 
-		req.on('data', function(chunk) {
-			total += chunk.length;
-			if (total >= bps) {
-				req.socket.pause();
-			}
-		});
+			req.on('data', function(chunk) {
+				total += chunk.length;
+				if (total >= bps) {
+					req.socket.pause();
+				}
+			});
 
-		req.on('end', function() {
-			clearInterval(pulse);
-		});
+			req.on('end', function() {
+				clearInterval(pulse);
+				req.socket.resume = resume;
+			});
+		}
 
 		next();
 	};
