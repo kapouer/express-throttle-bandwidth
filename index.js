a5bd78feb30cd7278fdaeefbaabb3a195c800271
Jérémy Lal <kapouer@melix.org>
2017-04-15 17:35:11 +0200

    Initial implementation

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = function(bps) {
+	return function(req, res, next) {
+		var total = 0;
+		var resume = req.socket.resume;
+		req.socket.resume = function() {};
+
+		var pulse = setInterval(function() {
+			total = total - bps / 100;
+			if (total < bps) {
+				resume.call(req.socket);
+			}
+		}, 10);
+
+		req.on('data', function(chunk) {
+			total += chunk.length;
+			if (total >= bps) {
+				req.socket.pause();
+			}
+		});
+
+		req.on('end', function() {
+			clearInterval(pulse);
+		});
+
+		next();
+	};
+};
+
